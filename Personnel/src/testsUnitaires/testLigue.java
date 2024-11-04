23c3295accaac0b2f026cee29e885cb01c5334e5
ASSAF Jafar <jassaf>
2024-11-04 14:43:38 +0100

    test unitaire setteur et getteur administrateur

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -24,4 +24,17 @@ class testLigue {
 				LocalDate.of(2023, 12, 01), LocalDate.of(2024, 12, 01));
 		assertEquals(employe, ligue.getEmployes().first());
 	}
+
+	@Test // test lorsqu'on supprimer un employé (ne devrait plus être présent après le remove() donc assertFalse)
+	void removeEmploye() throws SauvegardeImpossible {
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty", LocalDate.of(2023, 12, 01), LocalDate.of(2024, 12, 01));
+		
+		assertTrue(ligue.getEmployes().contains(employe));
+		
+		ligue.remove(employe);
+		
+		assertFalse(ligue.getEmployes().contains(employe));
+	}
 }
+
