52c2d291e460dac9760e3be882f493d098745053
FANOHARANTSOA Tojonirina <tfanoharantsoa@MDNPEDAGO.local>
2024-11-04 14:59:17 +0100

    création de l'exception date incohérente

---
+++ Personnel/src/personnel/DateIncoherente.java
@@ -0,0 +1,13 @@
+package personnel;
+
+public class DateIncoherente extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DateIncoherente(String message) {
+		super(message);
+	}
+
+}
